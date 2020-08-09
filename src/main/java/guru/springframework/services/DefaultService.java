b9d372245143004533ffa94bb00b5b3fdd25daf4
dunay2 <34416258+dunay2@users.noreply.github.com>
2020-08-09 21:55:11 +0100

    Added create recipe form. Service generalization.

--- src/main/java/guru/springframework/services/DefaultService.java
+++ src/main/java/guru/springframework/services/DefaultService.java
@@ -2,7 +2,7 @@ package guru.springframework.services;
 
 import java.util.Set;
 
-public interface BaseService <T,S> {
+public interface DefaultService<T,S> {
     T saveCommand(T command);
     T findCommandById(Long l);
     S findById(Long l);
